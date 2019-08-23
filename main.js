190b8e7a8b83b3d9ea6b5a5f0a9601c6bc78fee7
buhe <hare.bu@herewhite.com>
2019-08-23 18:05:54 +0800

    rm useless

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 const { app, BrowserWindow } = require('electron')
-const path = require("path");
 
 app.on('ready', () => {
-    // 新建一个窗口
     let mainWindow = new BrowserWindow({
         width: 1160,
         height: 720,
     });
-    // 原有的项目开发环境下的 devServer 的端口是 3000 ，我们这里以 url 形式把原有项目加载进来
     mainWindow.loadURL('https://demo-rtc.herewhite.com');
-    // mainWindow.loadFile(path.join(__dirname, "./build/index.html"));
 });
