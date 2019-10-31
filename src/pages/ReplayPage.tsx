57d39b9bf0b110a97920379bd13a302361da459a
伍双 <beingswu@gmail.com>
2019-10-31 22:15:50 +0800

    fix bug

--- src/pages/ReplayPage.tsx
+++ src/pages/ReplayPage.tsx
@@ -45,6 +45,7 @@ export default class ReplayPage extends React.Component<ReplayPageProps, Whitebo
     private startReplay = async (): Promise<void> => {
         const {userId, uuid, startTime, mediaUrl} = this.props.match.params;
         const roomToken = await this.getRoomToken(uuid);
+        const url = `https://netless-media.oss-cn-hangzhou.aliyuncs.com/${mediaUrl}`;
         if (roomToken) {
             this.netlessPlayer = WhiteFastSDK.Player("netless-replay", {
                 uuid: uuid,
@@ -65,7 +66,7 @@ export default class ReplayPage extends React.Component<ReplayPageProps, Whitebo
                 duration: this.getDuration(),
                 // mediaUrl: mediaUrl,
                 // isManagerOpen: true,
-                mediaUrl: "https://netless-media.oss-cn-hangzhou.aliyuncs.com/ad5ce237124d7210e24ca5838d79f509_b9fc17d5d017466ab446c3094c87b1b3.m3u8",
+                mediaUrl: url,
                 // isChatOpen:
             });
         }
