ac9808c3e28f9518e333fcd0d0e86685900c19af
leavesster <11785335+leavesster@users.noreply.github.com>
2020-11-10 13:41:59 +0800

    use global registry

--- build_and_push_image_edge.sh
+++ build_and_push_image_edge.sh
@@ -6,8 +6,8 @@ version=1.0.0
 
 hash=$(git rev-parse --short HEAD)
 
-docker build -f Dockerfile -t registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash -t registry.cn-hangzhou.aliyuncs.com/white/$image:latest .
-docker push registry.cn-hangzhou.aliyuncs.com/white/$image:$version-$hash
-docker push registry.cn-hangzhou.aliyuncs.com/white/$image:latest
+docker build -f Dockerfile -t registry-dev.netless.link/demo/$image:$version-$hash -t registry-dev.netless.link/demo/$image:latest .
+docker push registry-dev.netless.link/demo/$image:$version-$hash
+docker push registry-dev.netless.link/demo/$image:latest
 
-ssh k8s-site -tt "kubectl patch deployment demo-rtc -n site --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
+ssh k8s-site -tt "kubectl patch deployment demo-rtc -n demo --patch '{\"spec\": {\"template\": {\"metadata\": {\"annotations\": {\"version\": \"$version-$hash\"}}}}}'"
